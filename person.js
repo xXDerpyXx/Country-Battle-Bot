0139c153dbfb1353a0767bda6c0508166dbc8167
unknown <amir@eggwithlegs.com>
2020-03-26 09:56:58 +1300

    person script

--- person.js
+++ person.js
@@ -1,58 +1,104 @@
 module.exports = class Person {
 
-    constructor(id,x,y,culture) {
-        this.name = randomName(culture);
+	constructor(id, x, y, culture) {
+		
+		this.name = randomName(culture);
         this.id = id;
-        this.econStance = 0;
-        this.polStance = 0;
-        this.totalLifeSatisfaction = 0;
-        this.x = x;
+
+		this.econStance;
+		this.polStance = [];
+		this.totalLifeSatisfaction;
+
+		this.money = 0;
+
+		this.citizenship = [];
+		this.x = x;
         this.y = y;
 
-        this.money = 0;
-        this.citizenship = [];
+		this.happiness = 0;
 
-        this.happiness = 0;
-        this.inHand = null;
+		this.inHand = null;
         this.age = 0;
         this.culture = culture.name;
 
-        this.home = {"x":null,"y":null}
+		this.home = {"x":null,"y":null}
         this.work = {"x":null,"y":null}
 
-    }
+		this.isFirstTurn = true;
+
+	}
+
 }
 
-function pickLetter(type,c){
+function calculateTurn(individual) {
+	this.prevEconStance = individual.econStance;
+	this.prevPolStance = individual.polStance;
+
+	this.prevMoney = individual.money;
+
+	this.prevHappiness = individual.happiness;
+
+	//set happiness
+	//add to life satisfaction
+	//do other stuff too
+
+	this.totalLifeSatisfaction += (individual.happiness - 50) / individual.age; //this way if your happiness is less than average it reduces, but also the older you are the 
+	//less it impacts, idk if this is a good idea but yeah.
+
+	// If it is the first turn, there hasnt been a chance to calculate political stance based on happiness so instead it just gets set to whatever their country is.
+	if(individual.isFirstTurn) {
+		individual.polStance = individual.citizenship[0].calculatePolCompass()[0];
+		individual.econStance = individual.citizenship[0].calculatePolCompass()[1];
+	}
+
+
+	// If they are unhappy, their political views will change because they don't like their current leadership
+	if(prevHappiness <= 40) {
+		// If they are really unhappy with their government they will go to the opposite viewpoint
+		if(prevHappiness <= 15)
+			if(Math.random() == 1){
+				individual.polStance = -individual.polStance;
+				individual.econStance = -individual.econStance;
+			}
+		individual.polStance += (Math.random()*4)-2;
+		individual.econStance += (Math.random()*4)-2;
+	}
+
+	individual.polStance = clamp(individual.polStance, -10, 10);
+	individual.econStance = clamp(individual.econStance, -10, 10);
+
+}
+
+function pickLetter(type,c) {
     var constanants = c.constanants;
     var vowels = c.vowels;
-    if(type == "v"){
-        if(Math.random() > 0.5){
+    if(type == "v") {
+        if(Math.random() > 0.5) {
             return vowels[Math.floor(Math.random*vowels.length)]
-        }else{
+        } else {
             return constanants[Math.floor(Math.random*constanants.length)]
         }
-    }else{
-        if(Math.random() > 0.8){
+    } else {
+        if(Math.random() > 0.8) {
             return vowels[Math.floor(Math.random*vowels.length)]
-        }else{
+        } else {
             return constanants[Math.floor(Math.random*constanants.length)]
         }
     }
 }
 
-function randomName(c){
+function randomName(c) {
     var vowels = c.vowels;
     var output = "";
     var type = "c";
-    if(Math.random() > 0.5){
+    if(Math.random() > 0.5) {
         type = "v"
     }
     var letter = pickLetter(type,c);
-    for(var i = 0; i < (Math.random()*20)+1; i++){
-        if(vowels.includes(letter)){
+    for(var i = 0; i < (Math.random()*20)+1; i++) {
+        if(vowels.includes(letter)) {
             type = "v"
-        }else{
+        } else {
             type = "c"
         }
         output = output+""+letter;
@@ -62,34 +108,7 @@ function randomName(c){
 
 }
 
-function calculateTurn(individual) {
-    this.prevEconStance = individual.econStance;
-    this.prevPolStance = individual.polStance;
-
-    this.prevMoney = individual.money;
-
-    this.prevHappiness = individual.happiness;
-
-    //set happiness
-    //add to life satisfaction
-    //do other stuff too
-
-    this.totalLifeSatisfaction += (individual.happiness - 50) / individual.age; //this way if your happiness is less than average it reduces, but also the older you are the 
-    //less it impacts, idk if this is a good idea but yeah.
-
-    if(individual.isFirstTurn) {
-        individual.polStance = individual.citizenship[0].calculatePolCompass();
-    }
-
-    if(prevHappiness <= 40) {
-        if(prevHappiness <= 20)
-            if(Math.random() == 1)
-                individual.polStance = [-individual.polStance, -individual.polStance];
-        individual.polStance = [this.prevPolStance[0]+(Math.random()*2)-1, this.prevPolStance[1]+(Math.random()*2)-1];
-    }
-
-}
-
+// Clamp function cos js doesnt have it
 function clamp(val,min,max){
   if(val > max)
     val = max;
