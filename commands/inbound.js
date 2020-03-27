d55fcdd9cee9e1a65f4cc3ca4617b299429ca6a6
Derpy <bottledcanofdoom@gmail.com>
2020-03-27 12:39:04 -0400

    fixed in-bounds being dumb

--- commands/inbound.js
+++ commands/inbound.js
@@ -2,6 +2,6 @@ var v = require.main.require('./vars.js');
 
 module.exports = new v.c.cmd.Command(
     (args, msg) => {
-        return v.fn.map.oob(parseInt(content[1]),parseInt(content[2]));
+        return v.fn.map.oob(parseInt(args[1]),parseInt(args[2]));
     }
 );
