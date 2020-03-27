899404c910f3cb4510cbfbcc6bd4fdc078481754
Derpy <bottledcanofdoom@gmail.com>
2020-03-27 16:03:02 -0400

    whoops

--- commands/randomname.js
+++ commands/randomname.js
@@ -2,10 +2,10 @@ var v = require.main.require('./vars.js');
 
 module.exports = new v.c.cmd.Command(
     (args, msg) => {
-        if(v.d.worldData.cultures[args[0]] == null){
+        if(v.d.worldData.cultures[args.culture] == null){
             return "that isn't a culture"
         }
-        return v.fn.randomName(args[0]);
+        return v.fn.randomName(args.culture);
     },
 
     {
