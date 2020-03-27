a7076e00e0df58de880b1f515d116c35605aa779
flynnA <flynnamos@hotmail.com>
2020-03-27 17:37:58 +0000

    fix inbound.js
    make it work with the new argument system

--- commands/inbound.js
+++ commands/inbound.js
@@ -2,6 +2,23 @@ var v = require.main.require('./vars.js');
 
 module.exports = new v.c.cmd.Command(
     (args, msg) => {
-        return v.fn.map.oob(parseInt(args[1]),parseInt(args[2]));
+        return v.fn.map.oob(args.x,args.y);
+    },
+
+    {
+        args: [
+            new v.c.cmd.Argument(
+                'x',
+                {
+                    type: int
+                }
+            ),
+            new v.c.cmd.Argument(
+                'y',
+                {
+                    type: int
+                }
+            )
+        ]
     }
 );
