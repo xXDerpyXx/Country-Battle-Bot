207560784e7e4a6e24d076419a8fc6f356e32ae4
Derpy <bottledcanofdoom@gmail.com>
2020-03-27 13:01:59 -0400

    fixed the dumb random person not being found

--- commands/randomperson.js
+++ commands/randomperson.js
@@ -4,6 +4,9 @@ module.exports = new v.c.cmd.Command(
     (args, msg) => {
         var output = String();
         var p = v.fn.randomProperty(v.d.people);
+        if(p == undefined || p.id == undefined){
+            return 'No Person Found'
+        }
         for(let k in p){
             output += `   ${k}: ${p[k]}\n`;
         }
