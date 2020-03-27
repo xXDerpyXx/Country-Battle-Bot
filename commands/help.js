09eab59d7e843db22bda04f92f75fa09ffd4c31c
Flynn <flynnamos@hotmail.com>
2020-03-27 21:19:53 +0000

    Possible misinterpretations solved!
    they might have thought that the $help command didnt exist o.O

--- commands/help.js
+++ commands/help.js
@@ -19,7 +19,7 @@ module.exports = new v.c.cmd.Command(
                     argsDesc += `\`${i}\`${output[i]}`;
                 }
                 return `\`${v.d.settings.prefix}${args.cmd}${(Object.keys(output).length > 0 ? ` {${Object.keys(output).join('} {')}}` : ``)}\`: ${cmd.options.description}\n\n${argsDesc}`;
-            } else return 'That command doesn\'t exist.';
+            } else return 'You can\'t get help on a command which doesn\'t exist.';
         } else {
             let output = String();
             for (i of Object.keys(commands).sort()) {
