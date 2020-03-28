9b4bb742b2c04c2e6b8a6057be695b5e036978fc
flynnA <flynnamos@hotmail.com>
2020-03-28 18:10:36 +0000

    no more rewriting the name for every new funnyCulture

--- consts/funnyCultures.js
+++ consts/funnyCultures.js
@@ -1,43 +1,37 @@
-module.exports = {
+var cultures = {
     retard: {
         vowels:["a","e","u"],
         consonants:["r","t","d","h"],
-        name:"retard",
         capitalization:"random",
     },
 
     cabrikk: {
         vowels:["ā","ē","ī","ō","ū"],
         consonants:["ḃ", "ċ", "ḋ",  "ḳ", "ŀ", "ṅ", "ṗ", "ṛ"],
-        name:"cabrikk",
         capitalization:"start",
     },
 
     furry: {
         vowels:["u","o"],
         consonants:["w","x","3","n"],
-        name:"furry",
         capitalization:"start",
     },
 
     IRS: {
         vowels:["r","u"],
         consonants:["b","r"],
-        name:"IRS",
         capitalization:"none",
     },
 
     morse: {
         vowels:["·"," "],
         consonants:["-"],
-        name:"morse",
         capitalization:"none",
     },
 
     angery: {
         vowels:["e","a"],
         consonants:["g","h","!"],
-        name:"angery",
         capitalization:"all",
     },
 
@@ -45,8 +39,13 @@ module.exports = {
     gay: {
         vowels:["a","y"],
         consonants:["g"],
-        name:"gay",
         capitalization:"start",
     },
     */
+};
+
+for (let i in cultures) {
+    cultures[i].name = i;
 }
+
+module.exports = cultures;
