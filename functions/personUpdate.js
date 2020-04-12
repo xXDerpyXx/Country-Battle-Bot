83f636daaa2107b67239068fc4e3c769e2cb0ae9
Derpy <bottledcanofdoom@gmail.com>
2020-04-12 14:51:29 -0400

    people now like water

--- functions/personUpdate.js
+++ functions/personUpdate.js
@@ -14,10 +14,36 @@ module.exports = function personUpdate(id){
     for(var x = myx-1; x < myx+1; x++){
         for(var y = myy-1; x < myy+1; y++){
             if(v.fn.map.oob(x,y)){
-                localTiles.push(v.d.map[x][y]);
+                var temp = v.d.map[x][y];
+                temp.priority = 0
+                if(temp.elevation > v.d.mapInfo.seaLevel){ //this may change when boats exist
+                    localTiles.push(temp);
+                }
             }
         }
     }
+
+    var variance = 2;
+
+    for(var i = 0; i < localTiles.length;i++){
+        if(localTiles[i].elevation < here.elevation){
+            localTiles[i].priority += here.elevation - localTiles[i].elevation; 
+        }
+
+        localTiles[i].priority += (Math.random()*variance)-(variance/2)
+    }
+    for(var i = 0; i < localTiles.length;i++){
+        for(var j = 0; j < localTiles.length-1;j++){
+            if(localTiles[j].priority > localTiles[j+1].priority){
+                temp = localTiles[j];
+                localTiles[j] = localTiles[j+1];
+                localTiles[j+1] = temp;
+            }
+        }
+    }
+
+    rx = localTiles[0].x;
+    ry = localTiles[0].y;
 
     if(v.fn.map.oob(myx+rx,myy+ry)){ //If the location they're going to move to is within the boundaries of the map
         if(v.d.map[myx+rx][myy+ry].elevation > v.d.mapInfo.seaLevel){ //If the location the person is trying to move to is on land (and therefore not in the ocean)
